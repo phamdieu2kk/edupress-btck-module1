a185e39c91cfdf3b56a676cae166a2f98cb2226c
Pham Dieu <phamdieu1503@gmail.com>
2025-09-15 00:48:33 +0700

    fix api client

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,102 +1,110 @@
 // import axios from "axios";
 
-// const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-
 // const axiosClient = axios.create({
-//   baseURL,
+//   baseURL: "http://localhost:5000/api",
 //   headers: { "Content-Type": "application/json" },
 //   withCredentials: true,
 // });
 
-// // Log request & response để debug
-// axiosClient.interceptors.request.use((config) => {
-//   console.log("API Request:", config.method.toUpperCase(), config.url, config.data || config.params);
+// axiosClient.interceptors.request.use(config => {
+//   console.log("API Request:", config.url, config.data || config.params);
 //   return config;
 // });
 
-// axiosClient.interceptors.response.use(
-//   (response) => {
-//     console.log("API Response:", response.data);
-//     return response;
-//   },
-//   (error) => {
-//     console.error("API Error:", error.response?.data || error.message);
-//     return Promise.reject(error);
-//   }
-// );
+// axiosClient.interceptors.response.use(response => {
+//   console.log("API Response:", response.data);
+//   return response;
+// });
 
 // export default axiosClient;
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import axios from "axios";
+
+// // const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
+// // const axiosClient = axios.create({
+// //   baseURL,
+// //   headers: {
+// //     "Content-Type": "application/json",
+// //   },
+// //   withCredentials: true,
+// // });
+
+// // axiosClient.interceptors.request.use(
+// //   (config) => {
+// //     console.log("API Request:", config.method?.toUpperCase(), config.url, config.data || "");
+// //     return config;
+// //   },
+// //   (error) => Promise.reject(error)
+// // );
+
+// // axiosClient.interceptors.response.use(
+// //   (response) => {
+// //     console.log("API Response:", response.data);
+// //     return response.data;
+// //   },
+// //   (error) => {
+// //     console.error("API Response Error:", error.response?.data || error.message);
+// //     throw error;
+// //   }
+// // );
+
+// // export default axiosClient;
+
+
+// src/api/axiosClient.js
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL,
   headers: { "Content-Type": "application/json" },
-  withCredentials: true,
+  withCredentials: true, // nếu backend có cookie/session
 });
 
-axiosClient.interceptors.request.use(config => {
-  console.log("API Request:", config.url, config.data || config.params);
+// Log request để debug
+axiosClient.interceptors.request.use((config) => {
+  console.log(
+    "API Request:",
+    config.method?.toUpperCase(),
+    config.url,
+    config.data || config.params || ""
+  );
   return config;
 });
 
-axiosClient.interceptors.response.use(response => {
-  console.log("API Response:", response.data);
-  return response;
-});
+// Log response để debug
+axiosClient.interceptors.response.use(
+  (response) => {
+    console.log("API Response:", response.data);
+    return response;
+  },
+  (error) => {
+    console.error(
+      "API Error:",
+      error.response?.data || error.message
+    );
+    return Promise.reject(error);
+  }
+);
 
 export default axiosClient;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import axios from "axios";
-
-// const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-
-// const axiosClient = axios.create({
-//   baseURL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   withCredentials: true,
-// });
-
-// axiosClient.interceptors.request.use(
-//   (config) => {
-//     console.log("API Request:", config.method?.toUpperCase(), config.url, config.data || "");
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// axiosClient.interceptors.response.use(
-//   (response) => {
-//     console.log("API Response:", response.data);
-//     return response.data;
-//   },
-//   (error) => {
-//     console.error("API Response Error:", error.response?.data || error.message);
-//     throw error;
-//   }
-// );
-
-// export default axiosClient;
